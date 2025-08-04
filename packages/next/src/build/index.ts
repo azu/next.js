05b27c276293c815d202b08176f7f493f216affb
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-04 17:37:39 +0200

    remove console logs

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1264,22 +1264,15 @@ export default async function build(
 
         await nextBuildSpan
           .traceChild('copy-metadata-static-files')
-          .traceAsyncFn(() => {
-            console.log({
+          .traceAsyncFn(() =>
+            copyMetadataStaticFiles({
               appDir,
               pagesType: PAGE_TYPES.APP,
               pagePaths: appPaths,
               distDir,
               pageExtensions: config.pageExtensions,
             })
-            return copyMetadataStaticFiles({
-              appDir,
-              pagesType: PAGE_TYPES.APP,
-              pagePaths: appPaths,
-              distDir,
-              pageExtensions: config.pageExtensions,
-            })
-          })
+          )
 
         NextBuildContext.mappedAppPages = mappedAppPages
       }
